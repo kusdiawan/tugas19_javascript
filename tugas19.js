29e46c7a15f75e1e9a67cf1662c5de379625ae32
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-21 11:58:41 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,5 @@
+var kata = "Semangat....."
+
+var ulangKata = kata.repeat(5)
+
+console.log(ulangKata)
---
+++ tugas19_1.js
@@ -0,0 +1,28 @@
+var karakter = "*"
+
+var ulangChar = karakter.repeat(5)
+console.log(ulangChar)
+
+var ulangChar1 = karakter.repeat(15)
+console.log(ulangChar1)
+
+var ulangChar2 = karakter.repeat(25)
+console.log(ulangChar2)
+
+var ulangChar3 = karakter.repeat(50)
+console.log(ulangChar3)
+
+var ulangChar4 = karakter.repeat(80)
+console.log(ulangChar4)
+
+var ulangChar5 = karakter.repeat(50)
+console.log(ulangChar5)
+
+var ulangChar6 = karakter.repeat(25)
+console.log(ulangChar6)
+
+var ulangChar7 = karakter.repeat(15)
+console.log(ulangChar7)
+
+var ulangChar8 = karakter.repeat(5)
+console.log(ulangChar8)
